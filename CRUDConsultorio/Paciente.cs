bec57d21006bf69205c5208c9968132ff4a0945f
Milka Freire <milkafreire40@gmail.com>
2025-02-11 15:25:27 -0400

    Criação da classe paciente

---
+++ CRUDConsultorio/Paciente.cs
@@ -0,0 +1,10 @@
+﻿public class Paciente
+{
+    public int IdPaciente { get; set; }
+    public string Nome { get;set;}
+    public string Cpf { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+    public DateOnly DataNascimento { get; set; }
+
+}
